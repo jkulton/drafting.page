d7e9438f2fc7bb1c468188f1bb0154d83b4d9129
Jon Kulton <jonkulton@gmail.com>
2022-11-03 23:44:20 -0400

    cleanup app.js, set to light mode

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 import {EditorState} from "@codemirror/state"
 import {EditorView, keymap, highlightActiveLine} from "@codemirror/view"
 import {defaultKeymap} from "@codemirror/commands"
-import { oneDark } from '@codemirror/theme-one-dark';
 
 document.addEventListener('DOMContentLoaded', () => {
   const editorElement = document.body.querySelector('#editor');
-  let saveDebounce;
   const existingContent = localStorage.getItem('__content');
+  let saveDebounce;
   
   function updateHandler(event) {
     if (!event.docChanged) {
@@ -26,7 +25,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }, 500);
   }
   
-  let startState = EditorState.create({
+  const startState = EditorState.create({
     doc: existingContent || '',
     extensions: [
       keymap.of(defaultKeymap),
@@ -34,12 +33,10 @@ document.addEventListener('DOMContentLoaded', () => {
       EditorView.updateListener.of(updateHandler),
       oneDark,
     ]
-  })
+  });
   
-  let view = new EditorView({
+  const view = new EditorView({
     state: startState,
     parent: editorElement
   });
-
-  view.highlightActiveLine();
 });
